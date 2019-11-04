a16efce72f976b1f2d6d4d76d1497194e0bca0c6
joonyong.kim <joonyong.kim@joonyongkim>
2019-11-04 18:38:09 +0900

    chap 5

--- src/main/java/com/lucern/Application.java
+++ src/main/java/com/lucern/Application.java
@@ -1,19 +1,47 @@
 package com.lucern;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
+
+@EnableResourceServer
+@EnableAuthorizationServer
+@SpringBootApplication
 @EnableJpaRepositories("com.lucern.domain.*")
 @ComponentScan(basePackages = { "com.lucern.domain.*" })
 @EntityScan("com.lucern.domain.*")   
-@SpringBootApplication
-public class Application {
+
+public class Application extends ResourceServerConfigurerAdapter{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.headers().frameOptions().disable();
+		http.authorizeRequests()
+			.anyRequest().permitAll()
+			.antMatchers("/authorization-code-test").access("#oauth2.hasScope('read')");
+	}
+	
+	@Bean
+	public TokenStore JdbcTokenStore(DataSource dataSource) {
+		return new JdbcTokenStore(dataSource);
+	}
+
+	
 
 }
--- src/test/java/com/lucen/ApplicationTests.java
+++ src/test/java/com/lucen/ApplicationTests.java
@@ -1,9 +1,15 @@
 package com.lucen;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
+import com.lucern.Application;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
 class ApplicationTests {
 
 	@Test
